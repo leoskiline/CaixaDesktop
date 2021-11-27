4b7bfafe1cc19137a4f6451d6c4847466b525b15
Leonardo Custodio <44417666+leoskiline@users.noreply.github.com>
2021-11-27 15:49:17 -0300

    ok

--- Teste/Teste.cs
+++ Teste/Teste.cs
@@ -16,18 +16,6 @@ namespace Teste
             set { testContextInstance = value; }
         }
 
-        string GetRelativePath(string filespec, string folder)
-        {
-            Uri pathUri = new Uri(filespec);
-            // Folders must end in a slash
-            if (!folder.EndsWith(Path.DirectorySeparatorChar.ToString()))
-            {
-                folder += Path.DirectorySeparatorChar;
-            }
-            Uri folderUri = new Uri(folder);
-            return Uri.UnescapeDataString(folderUri.MakeRelativeUri(pathUri).ToString().Replace('/', Path.DirectorySeparatorChar));
-        }
-
         [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
         "C:\\Users\\Leonardo\\source\\repos\\CaixaDesktop\\testes.csv", "testes#csv",
         DataAccessMethod.Sequential), DeploymentItem("testes.csv"), TestMethod]
